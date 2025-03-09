77efa029a47cf092a2c0eccfaa69f837f10a8bf7
ETong0 <et2666@nyu.edu>
2025-03-08 23:05:52 -0500

    screen responsive

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction(){
+    var x = document.getElementById("items");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+if (window.innerWidth > 800){
+    x.style.display = "none";
+  }
