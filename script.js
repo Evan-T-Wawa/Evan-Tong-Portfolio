8a430ac61bc36a82c1a32e8cf4a8520939c52592
ETong0 <et2666@nyu.edu>
2025-03-11 12:10:07 -0400

    nav bar menu buttom change colour

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-
-
 function myFunction(){
   var x = document.getElementById("items");
+  var y = document.getElementById("nav-button-open");
+  var z = document.getElementById("nav-button-closed");
+
   if (x.style.display === "block") {
     x.style.display = "none";
+    z.style.display = "block";
+    y.style.display = "none";
+
   } else {
     x.style.display = "block";
+    z.style.display = "none";
+    y.style.display = "block";
   }
 }
 
